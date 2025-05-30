63cd0dde5397acaf13a3af3f6f994ab1f4494571
MihkelPLoompuu <158562003+MihkelPLoompuu@users.noreply.github.com>
2025-05-30 13:10:43 +0300

    https://mihkelploompuu-1747115538632.atlassian.net/browse/KAN-70
    Tegin Watchlisti

---
+++ Filmiregister/Controllers/WatchlistController.cs
@@ -0,0 +1,38 @@
+﻿using Filmiregister.Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Filmiregister.Controllers
+{
+    public class WatchlistController : Controller
+    {
+        private static List<Film> _watchlist = new List<Film>();
+        public IActionResult Index()
+        {
+            return View(_watchlist);
+        }
+        [HttpGet]
+        public IActionResult Add(Guid id, string title)
+        {
+            var movie = new Film { FilmID = id, FilmTitle = title };
+
+            if (!_watchlist.Any(m => m.FilmID == id))
+            {
+                _watchlist.Add(movie);
+            }
+
+            return RedirectToAction("Index");
+        }
+        [HttpPost]
+        public IActionResult Remove(Guid id)
+        {
+            var movie = _watchlist.FirstOrDefault(m => m.FilmID == id);
+            if (movie != null)
+            {
+                _watchlist.Remove(movie);
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
+}
